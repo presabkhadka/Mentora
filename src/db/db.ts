d374174f50b2253231ea698e89321d81bcfc7382
Presab <presabkhadka30@gmail.com>
2025-06-19 12:14:23 +0545

    int - initializing the project and creating db schemas

---
+++ src/db/db.ts
@@ -0,0 +1,66 @@
+import mongoose, { mongo, Mongoose } from "mongoose";
+import { ref } from "process";
+
+mongoose.connect(
+  "mongodb+srv://presabkhadka30:fRANqisUqmoJ5AkK@cluster0.g6wpo.mongodb.net/mentora"
+);
+
+const contentTypes = ["image", "video", "article", "audio"];
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const contentSchema = new mongoose.Schema({
+  link: {
+    type: String,
+    require: true,
+  },
+  type: {
+    type: String,
+    enum: contentTypes,
+  },
+  title: {
+    type: String,
+    require: true,
+  },
+  tags: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Tags",
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+  },
+});
+
+const tagSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+});
+
+const linkSchema = new mongoose.Schema({
+  hash: {
+    type: String,
+    require: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+  },
+});
+
+export const Users = mongoose.model("Users", userSchema);
+export const Content = mongoose.model("Content", contentSchema);
+export const Tags = mongoose.model("Tags", tagSchema);
+export const Link = mongoose.model("Link", linkSchema);
+
+module.exports({
+  Users,
+  Content,
+  Tags,
+  Link,
+});
