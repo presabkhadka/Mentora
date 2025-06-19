322eca99b0e263a03a1998d1240b8b49dad9e301
Presab <presabkhadka30@gmail.com>
2025-06-19 12:48:31 +0545

    updt - initializing user router in the root file

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { userRouter } from "./routes/userRoutes";
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const app = express();
 const port = process.env.port;
 app.use(express.json());
 app.use(cors());
+
+app.use("/user", userRouter);
 
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
